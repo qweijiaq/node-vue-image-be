49e044431b186fda0be61878e1075817a7ad97e4
WeiJia <2112025700@qq.com>
2023-06-04 19:48:57 +0800

    增加了内容方面的三个接口

---
+++ note.md
@@ -0,0 +1,9 @@
+创建内容： app.post('posts', handler)
+
+更新内容： app.patch('posts/:postId', handler)
+
+删除内容： app.delete('posts/:postId', handler)
+
+获取内容列表：app.get('posts', handler)
+
+获取单个内容： app.get('posts/:postId', handler)
--- src/main.js
+++ src/main.js
@@ -2,5 +2,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/*
+* 使用 JSON 中间件
+*/
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`🚀 服务已启动！`))
+
+const data = [
+    {
+        id: 1,
+        title: '第一',
+        content: '第一个内容'
+    },
+    {
+        id: 2,
+        title: '第二',
+        content: '第二个内容'
+    },
+    {
+        id: 3,
+        title: '第三',
+        content: '第三个内容'
+    }
+]
+
+/*
+* 获取内容列表
+*/
+app.get('/posts', (req, res) => {
+    res.send(data)
+})
+
+/*
+* 获取单个内容
+*/
+app.get('/posts/:postId', (req, res) => {
+    const { postId } = req.params
+    const post = data.filter(item => item.id == postId)
+    res.send(post[0])
+})
+
+/*
+* 创建内容
+*/
+app.post('/posts', (req, res) => {
+    // 获取请求里的数据
+    const { content } = req.body
+    // 创建响应状态码
+    res.statusCode = 201
+    // 作出响应
+    res.send({
+        message: `成功创建了内容: ${content}`
+    })
+})
