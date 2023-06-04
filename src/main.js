1205d93f332d884d9e74bd285a9d1a276cc3df1e
WeiJia <2112025700@qq.com>
2023-06-04 15:23:27 +0800

    规定 git 忽略文件 && 创建 Web 服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-const greeting = "hello ~"
-console.log(greeting)
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.write('hello')
+    res.end()
+})
+
+server.listen(3000, () => {
+    console.log('🚀 服务已启动！')
+})
